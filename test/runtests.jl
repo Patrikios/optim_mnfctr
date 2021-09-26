93dc558d3583811ad51e8c2cf80f610694bc9ca6
Patrikios <Patrikios@users.noreply.github.com>
2021-09-26 12:59:55 +0200

    Commit 3 on 2021-09-26

--- test/runtests.jl
+++ test/runtests.jl
@@ -62,12 +62,7 @@ end
 
 
 #=
-
-using TravelingSalesmanExact, GLPK
-set_default_optimizer!(GLPK.Optimizer)
-n = 50
-cities = [ 100*rand(2) for _ in 1:n];
-tour, cost = get_optimal_tour(cities; verbose = true)
-plot_cities(cities[tour])
-
+To DO
+- compare solutions with package TravellingsalesemanExact 
+- add heuristucs for non-exact solution (n > 13)
 =#
